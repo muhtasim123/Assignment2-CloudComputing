c67bd90cb8b6d1d5e92cbc9b22b3a1c55e06d702
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2020-02-08 01:06:15 +0000

    adding content to the login page

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 
 <?php
 	$myPDO = new PDO('pgsql:host=ec2-35-172-85-250.compute-1.amazonaws.com', 'dbname=d7h71ne38f3nm8', 'rvauqwbiucrruu', '5fa2b549d88496011b16dfd7e548f3b4ec17f9452123f32c1c34d60a80f37946');
-	$result = $myPDO->query("SELECT 
+
+	$thisusername = trim($_POST["uname"]);
+	$thispassword = trim($_POST["psw"]);
+
+	$result = $myPDO->query("SELECT * FROM assignment WHERE username = '$thisusername' AND password = '$thispassword';";
 
 	if($_SERVER["REQUEST METHOD"] == "POST"){
-		$thisusername = trim($_POST["uname"]);
-		$thispassword = trim($_POST["psw"]);
-
 		if(pg_num_rows($result) == 1) {
 			header("location: upload.php");
 		} else {
