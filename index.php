e3041e83989beef1f3d43fbda4f8e4beeea235c6
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2020-02-08 02:21:14 +0000

    adding content to the login page

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
-<?php
-
-	$thisusername = trim($_POST["uname"]);
-
-	$thispassword = trim($_POST["psw"]);
-
-
-
-	$conn_string = "host=ec2-35-172-85-250.compute-1.amazonaws.com port=5432 dbname=d7h71ne38f3nm8 user=rvauqwbiucrruu password=5fa2b549d88496011b16dfd7e548f3b4ec17f9452123f32c1c34d60a80f37946";
-
-	$dbconn = pg_connect($conn_string);
-
-
-
-	$result = pg_query($dbconn, “SELECT * FROM assignment WHERE username = '$thisusername' AND password = '$thispassword';");
-
-	if($_SERVER["REQUEST METHOD"] == "POST"){
-
-		if(pg_num_rows($result) == 1) {
-
-			header("location: upload.php");
-
-		} else {
-
-			$error = "Your login username or password is invalid";
-
-		}
-
-	}
-
-?>
-
+<!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -75,7 +44,7 @@ span.psw {
 
 <h2>Login Form</h2>
 
-<form action=" " method="post">
+<form action="/action_page.php" method="post">
 
 
   <div class="container">
@@ -83,7 +52,8 @@ span.psw {
     <input type="text" placeholder="Enter Username" name="uname" required>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required> 
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
     <button type="submit">Login</button>
   </div>
 
@@ -91,3 +61,4 @@ span.psw {
 
 </body>
 </html>
+
