07762beeadf1fc6a6557f8512859f4932ce8a6de
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2020-02-09 04:57:59 +0000

    changing the styling of upload.php

--- upload.php
+++ upload.php
@@ -1,29 +1,67 @@
 <?php
-require('vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+    require('vendor/autoload.php');
+    // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+    $s3 = Aws\S3\S3Client::factory();
+    $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
-    <head><meta charset="UTF-8"></head>
-    <body>
-        <h1>Assignment 2! </h1>
-		
-		<a href="https://assignment2-muhtasim.herokuapp.com/download.php">Files List</a>
+<head><meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {box-sizing: border-box}
+        body {font-family: "Lato", sans-serif;}
+
+        /* Style the tab */
+        .tab {
+            float: left;
+            border: 1px solid #ccc;
+            background-color: #f1f1f1;
+            width: 30%;
+            height: 300px;
+        }
+
+        /* Style the buttons inside the tab */
+        .tab button {
+            display: block;
+            background-color: inherit;
+            color: black;
+            padding: 22px 16px;
+            width: 100%;
+            border: none;
+            outline: none;
+            text-align: left;
+            cursor: pointer;
+            transition: 0.3s;
+            font-size: 17px;
+        }
+
+        /* Change background color of buttons on hover */
+        .tab button:hover {
+            background-color: #ddd;
+        }
+
+    </style>
+</head>
+<body>
+<h1>Assignment 2! </h1>
+<div class="tab">
+    <button class="tablinks" onclick="/upload.php">Upload</button>
+    <button class="tablinks" onclick="/download.php">Download</button>
+</div>
+<a href="https://assignment2-muhtasim.herokuapp.com/download.php">Files List</a>
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-    // FIXME: add more validation, e.g. using ext/fileinfo
-    try {
-        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
-<?php } catch(Exception $e) { ?>
-        <p>Upload error :(</p>
-<?php } } ?>
-        <h2>Upload a file</h2>
-        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-            <input name="userfile" type="file"><input type="submit" value="Upload">
-        </form>
-    </body>
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+        // FIXME: add more validation, e.g. using ext/fileinfo
+        try {
+            // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+            $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+            ?>
+            <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+        <?php } catch(Exception $e) { ?>
+            <p>Upload error :(</p>
+        <?php } } ?>
+<h2>Upload a file</h2>
+<form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input name="userfile" type="file"><input type="submit" value="Upload">
+</form>
+</body>
 </html>
