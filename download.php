f97ccba4a57b589dc6ba5b0a53b82f82e74d0f80
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2020-02-09 05:19:11 +0000

    fixing the style for download.php

--- download.php
+++ download.php
@@ -1,26 +1,64 @@
 <?php
-require('vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+    require('vendor/autoload.php');
+    // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+    $s3 = Aws\S3\S3Client::factory();
+    $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
-    <head><meta charset="UTF-8"></head>
-    <body>
-        <h1>Files Available for Dwonload</h1>
-<?php
-	try {
-		$objects = $s3->getIterator('ListObjects', array(
-			"Bucket" => $bucket
-		));
-		foreach ($objects as $object) {
-?>
-		<p> <a href="<?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?>"> <?echo $object['Key'] . "<br>";?></a></p>
-		
-<?		}?>
+<head><meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        * {box-sizing: border-box}
+        body {font-family: "Lato", sans-serif;}
 
-<?php } catch(Exception $e) { ?>
+        /* Style the tab */
+        .tab {
+            float: left;
+            border: 1px solid #ccc;
+            background-color: #f1f1f1;
+            width: 18%;
+        }
+
+        /* Style the buttons inside the tab */
+        .tab button {
+            display: block;
+            background-color: inherit;
+            color: black;
+            padding: 22px 16px;
+            width: 100%;
+            border: none;
+            outline: none;
+            text-align: left;
+            cursor: pointer;
+            transition: 0.3s;
+            font-size: 17px;
+        }
+
+        /* Change background color of buttons on hover */
+        .tab button:hover {
+            background-color: #ddd;
+        }
+
+    </style></head>
+<body>
+<div class="tab">
+    <h1>Assignment 2</h1>
+    <button class="tablinks" onclick="window.location.href = 'https://assignment2-muhtasim.herokuapp.com/upload.php';">Upload</button>
+    <button class="tablinks" onclick="window.location.href = 'https://assignment2-muhtasim.herokuapp.com/download.php';">Download</button>
+</div>
+<h1>Files Available for Download</h1>
+<?php
+    try {
+        $objects = $s3->getIterator('ListObjects', array(
+            "Bucket" => $bucket
+        ));
+        foreach ($objects as $object) {
+            ?>
+            <p> <a href="<?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?>"> <?echo $object['Key'] . "<br>";?></a></p>
+
+        <?		}?>
+
+    <?php } catch(Exception $e) { ?>
         <p>error :(</p>
-<?php }  ?>
-    </body>
+    <?php }  ?>
+</body>
 </html>
