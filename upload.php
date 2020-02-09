e1e9460449247a76319d516a93b9a52faf3b9281
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2020-02-09 05:11:04 +0000

    changing the styling of upload.php

--- upload.php
+++ upload.php
@@ -44,7 +44,7 @@
 <div class="tab">
 	<h1>Assignment 2</h1>
     <button class="tablinks" onclick="/upload.php">Upload</button>
-    <button class="tablinks" onclick="https://assignment2-muhtasim.herokuapp.com/download.php">Download</button>
+    <button class="tablinks" onclick="window.location.href = 'https://assignment2-muhtasim.herokuapp.com/download.php';">Download</button>
 </div>
 <a href="https://assignment2-muhtasim.herokuapp.com/download.php">Files List</a>
 <?php
