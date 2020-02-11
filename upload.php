095ca2cb124aa70fba4ed3f142b424d931d2ead1
muhtasim123 <muhtasim.chowdhury@uoit.net>
2020-02-11 13:39:19 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -57,9 +57,7 @@
 <div class="other">
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-        // FIXME: add more validation, e.g. using ext/fileinfo
         try {
-            // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
             $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
             ?>
             <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
